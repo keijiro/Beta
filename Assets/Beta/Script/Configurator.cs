73b8d4d5aa10dc70850cbf03595adfcacd32b0ab
Keijiro Takahashi <keijiro@gmail.com>
2017-04-21 00:31:19 +0900

    Multi-display

---
+++ Assets/Beta/Script/Configurator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Beta
+{
+    public class Configurator : MonoBehaviour
+    {
+        [SerializeField] Camera _mainCamera;
+
+        void Start()
+        {
+            #if !UNITY_EDITOR
+
+            // Enable multi-display mode.
+            if (Display.displays.Length > 1)
+            {
+                Display.displays[0].Activate();
+                Display.displays[1].Activate();
+                _mainCamera.targetDisplay = 1;
+            }
+
+            #endif
+        }
+    }
+}
