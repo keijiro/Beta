c9f0ceb0adb1ead5149ae2d172ac0e9ed5e1bcac
Keijiro Takahashi <keijiro@gmail.com>
2018-02-17 19:47:18 +0900

    Updated with 2017.3.1

--- Assets/Klak/Wiring/Editor/Patcher/NodeEditor.cs
+++ Assets/Klak/Wiring/Editor/Patcher/NodeEditor.cs
@@ -44,6 +44,11 @@ namespace Klak.Wiring.Patcher
             DestroyImmediate(_editor);
         }
 
+        public override bool RequiresConstantRepaint()
+        {
+            return _editor != null ? _editor.RequiresConstantRepaint() : false;
+        }
+
         protected override void OnHeaderGUI()
         {
             var node = (Node)target;
